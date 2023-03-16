da8798e424a83ced1d08d9e8b8252872afd75cc2
Davi Lauritzen <davi.lauritzen1@gmail.com>
2023-03-16 14:10:35 -0300

    Initial Commit

---
+++ src/application/program.java
@@ -0,0 +1,53 @@
+package application;
+
+import entities.Employee;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter full file path: ");
+        String path = sc.next();
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            List<Employee> list = new ArrayList<>();
+            String line = br.readLine();
+            while (line != null) {
+                String[] fields = line.split(",");
+                list.add(new Employee(fields[0], fields[1], Double.parseDouble(fields[2])));
+                line = br.readLine();
+            }
+
+            System.out.println("Enter salary: ");
+            double salary = sc.nextDouble();
+
+            List<String> email = list.stream()
+                    .filter(x -> x.getSalary() > salary)
+                    .map(x -> x.getEmail())
+                    .sorted()
+                    .collect(Collectors.toList());
+            System.out.println("Email of people whose salary is more than " + salary + ":");
+            email.forEach(System.out::println);
+
+            double sum = list.stream()
+                    .filter(x -> x.getName().charAt(0) == 'M')
+                    .map(x -> x.getSalary())
+                    .reduce(0.0, (x, y) -> x + y);
+
+            System.out.println("Sum of salary from people whose name starts with 'M': " + String.format("%.2f", sum));
+
+        } catch (Exception e) {
+            e.getMessage();
+            e.printStackTrace();
+        }
+
+
+    }
+}
